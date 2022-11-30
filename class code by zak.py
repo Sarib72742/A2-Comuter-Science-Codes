edd60423ec7c0f5427f3cfba3c9afa6ae4671a82
Muhammad Sarib <93588162+Sarib72742@users.noreply.github.com>
2022-11-30 18:50:21 +0500

    Add files via upload

---
+++ class code by zak.py
@@ -0,0 +1,20 @@
+def unknown(x,y):
+    value = 1
+    while (x != y):
+        if x < y:
+            print(x+y)
+            value = (value * 2)
+            x = x+1
+        else:
+            print(x+y)
+            value = (value // 2)
+            x = x-1
+    return value
+
+
+
+print(unknown(10,15))
+
+print(unknown(10,10))
+
+print(unknown(15,10))
