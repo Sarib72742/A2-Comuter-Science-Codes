b1979f6a1fda07a01acfbc2334ee7d5f539bfb06
Muhammad Sarib <93588162+Sarib72742@users.noreply.github.com>
2023-03-17 08:03:38 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
-baseptr = topofstack = 0
-stack = []
+class stacklist():
+    def __init__(self):
+        self.data = ""
+        self.pointer = 0
+
+freeptr = tos = baseptr = 0
+stack =[stacklist() for i in range(5)]
 
 def initiallise():
-    global baseptr, topofstack, stack
-    topofstack = 0
-    baseptr = -1
-    stack = [None for i in range(0,5)]
+    global freeptr, tos, stack, stacklist
+    freeptr = 0
+    tos = -1
+    for index in range(5):
+        stack[index].pointer = index +1
+    stack[4].pointer = -1
 
-def push (value):
-    global baseptr, topofstack, stack
-    if topofstack < 5:
-        stack[topofstack] = value
-        topofstack +=1
+def push(value):
+    global freeptr, tos, stack, stacklist
+    if freeptr == -1:
+        print("no space avaliable")
     else:
-        print("overflow")
+        newnode = freeptr
+        stack[newnode].data = value
+        freeptr = stack[freeptr].pointer
+        stack[newnode].pointer = tos
+        tos = newnode
 
 def pop():
-    global baseptr, topofstack, stack
-    if topofstack == baseptr + 1:
-        print("underflow stack is empty")
+    global freeptr, tos, stack, stacklist
+    if tos == -1:
+        print("no data exist")
     else:
-        topofstack -= 1
-        value = stack[topofstack]
-        stack[topofstack] = None
+        value = stack[tos].data
+        stack[tos].pointer = freeptr
+        stack[tos].data = None
+        freeptr = tos
+        tos = tos-1
     return value
 
+def post_order_traversal(tos):
+    if tos is not None:
+        post_order_traversal(stack[tos].pointer)
+        print(stack[tos].data)
+
 def outputstack():
-    global baseptr, topofstack, stack
+    global freeptr, tos, stack, stacklist
     for i in range(5):
-        print(i, stack[i])
+        print(i, stack[i].data, stack[i].pointer)
 
 initiallise()
 print("1:push\n2:pop\n3:output\n4:End")
@@ -37,6 +54,7 @@ while choice != 4:
     if choice == 1:
         data = input("enter value")
         push(data)
+        post_order_traversal(tos)
     elif choice == 2:
         value = pop()
         print("value pop is", value)
@@ -44,3 +62,5 @@ while choice != 4:
         outputstack()
 
     choice = int(input("please enter your choice"))
+
+
